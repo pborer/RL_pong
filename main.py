2e697ea04c6772a31dde09016f2f17e0149ebbab
Perry <perryborer@hotmail.com>
2021-10-31 12:20:23 +0000

    Added a human-controllable bat

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Pong via reinforcement learning')
 clock = pygame.time.Clock()
-appExit = False
 
+
+# Config
+bat_acceleration = 5
 
 # Define objects
 class Ball:
@@ -32,33 +34,75 @@ class Ball:
         self.centre_y_pos = self.centre_y_pos + self.y_vel
 
     def handle_bounce(self):
-        ball_left_edge = self.centre_x_pos - self.radius
-        ball_right_edge = self.centre_x_pos + self.radius
-        ball_top_edge = self.centre_y_pos - self.radius
-        ball_bottom_edge = self.centre_y_pos + self.radius
+        left_edge_pos = self.centre_x_pos - self.radius
+        right_edge_pos = self.centre_x_pos + self.radius
+        top_edge_pos = self.centre_y_pos - self.radius
+        bot_edge_pos = self.centre_y_pos + self.radius
 
-        if ball_left_edge <= 0:
+        if left_edge_pos <= 0:
             self.x_vel *= -1
-            self.centre_x_pos += -ball_left_edge
+            self.centre_x_pos += -left_edge_pos
 
-        elif ball_right_edge >= window_width:
+        elif right_edge_pos >= window_width:
             self.x_vel *= -1
-            self.centre_x_pos += ball_right_edge - window_width
+            self.centre_x_pos += right_edge_pos - window_width
 
-        if ball_top_edge <= 0:
+        if top_edge_pos <= 0:
             self.y_vel *= -1
-            self.centre_y_pos += -ball_top_edge
+            self.centre_y_pos += -top_edge_pos
 
-        elif ball_bottom_edge >= window_height:
+        elif bot_edge_pos >= window_height:
             self.y_vel *= -1
-            self.centre_y_pos += ball_bottom_edge - window_height
+            self.centre_y_pos += bot_edge_pos - window_height
 
     def draw(self):
         pygame.draw.circle(window, self.color, (self.centre_x_pos, self.centre_y_pos), self.radius)
 
 
+
+class Bat:
+    def __init__(self, top_edge_pos, bot_edge_pos, left_edge_pos, right_edge_pos):
+        self.top_edge_pos = top_edge_pos
+        self.bot_edge_pos = bot_edge_pos
+        self.left_edge_pos = left_edge_pos
+        self.right_edge_pos = right_edge_pos
+        self.acceleration = bat_acceleration
+        self.x_velocity = 0
+
+    def accelerate(self, direction):
+        if direction == "left":
+            self.x_velocity -= self.acceleration
+        elif direction == "right":
+            self.x_velocity += self.acceleration
+
+    def decelerate(self, direction):
+        if direction == "left":
+            self.x_velocity += self.acceleration
+        elif direction == "right":
+            self.x_velocity -= self.acceleration
+
+    def move(self):
+        bat_width = self.right_edge_pos - self.left_edge_pos
+        self.left_edge_pos += self.x_velocity
+        self.right_edge_pos += self.x_velocity
+        if self.left_edge_pos < 0:
+            self.left_edge_pos = 0
+            self.right_edge_pos = bat_width
+        elif self.right_edge_pos > window_width:
+            self.right_edge_pos = window_width
+            self.left_edge_pos = window_width - bat_width
+
+    def draw(self):
+        rectangle = pygame.Rect(self.left_edge_pos, self.top_edge_pos,
+                                self.right_edge_pos - self.left_edge_pos,
+                                self.bot_edge_pos - self.top_edge_pos)
+        pygame.draw.rect(window, WHITE, rectangle)
+
+
 # Initial setup
 ball = Ball((window_width / 2), (window_height / 2), 10)
+bot_bat = Bat(window_height - 10, window_height, window_width / 2 - 40, window_width / 2 + 40)
+appExit = False
 
 
 # Application loop
@@ -70,12 +114,26 @@ while not appExit:
             pygame.quit()
             quit()
 
+        # Move the bottom bat left or right if commanded by the human player
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                bot_bat.accelerate("left")
+            if event.key == pygame.K_RIGHT:
+                bot_bat.accelerate("right")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                bot_bat.decelerate("left")
+            if event.key == pygame.K_RIGHT:
+                bot_bat.decelerate("right")
+
+    bot_bat.move()
     ball.move()
     ball.handle_bounce()
 
     # Update display
     window.fill(BLACK)
     ball.draw()
+    bot_bat.draw()
     pygame.display.update()
     clock.tick(60)
 
